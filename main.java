24eef700e4afb89cb67581c9d2b103784e706332
Dduquejh <johandanielduquehernandez@gmail.com>
2023-08-13 19:24:51 -0500

    Se completa la clase Main

--- main.java
+++ main.java
@@ -1,4 +1,30 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
+        pantallaBienvenida();
+        Repl.starRepel();
+    }
+
+    public static void pantallaBienvenida() {
+        System.out.println("Bienvenido al lenguaje");
+        String[] letras = {
+            "*****", "*    ", "*****", "*    ", "*    ", "*****",   // E
+            "**** ", "*   *", "**** ", "*  * ", "*   *", "*   *"    // R
+        };
+
+        String mensaje = "ERE";
+        for (int fila = 0; fila < 6; fila++) {
+            for (char letra : mensaje.toCharArray()) {
+                int indice;
+                if (letra == 'E') {
+                    indice = 0;
+                } else if (letra == 'R') {
+                    indice = 1;
+                } else {
+                    continue; // Caracter no reconocido, omite
+                }
+                System.out.print(letras[indice * 6 + fila] + "  ");
+            }
+            System.out.println();
+        }
     }
 }
