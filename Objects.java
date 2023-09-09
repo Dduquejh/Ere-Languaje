3b1c03d72d367bcd4a4999027e1023c825133e68
throwmen <j.oliveros@ieciudaddonbosco.edu.co>
2023-09-09 15:27:32 -0500

    Creación de la clase y el resto de clases para cada objeto

--- Objects.java
+++ Objects.java
@@ -1,4 +1,4 @@
-enum objectType {
+enum ObjectType {
     BOOLEAN,
     INTEGER,
     NULL,
@@ -6,6 +6,50 @@ enum objectType {
     STRING
 }
 
-public class Objects {
+abstract class CustomObjects {
+    public abstract ObjectType type();
 
+    public abstract String inspect();
+}
+
+class IntegerObject extends CustomObjects {
+    int value;
+
+    public IntegerObject(int value) {
+        this.value = value;
+    }
+
+    public ObjectType type() {
+        return ObjectType.INTEGER;
+    }
+
+    public String inspect() {
+        return String.valueOf(value);
+    }
+}
+
+class BooleanObject extends CustomObjects {
+    boolean value;
+
+    public BooleanObject(boolean value) {
+        this.value = value;
+    }
+
+    public ObjectType type() {
+        return ObjectType.BOOLEAN;
+    }
+
+    public String inspect() {
+        return String.valueOf(value);
+    }
+}
+
+class Null extends CustomObjects {
+    public ObjectType type() {
+        return ObjectType.NULL;
+    }
+
+    public String inspect() {
+        return "nulo";
+    }
 }
